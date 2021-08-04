1f1ff30bf4c94af448bc162c9862828807d08cb1
CuongNC <you@example.com>
2021-08-05 01:42:58 +0700

    create state constant, change port in prop, add dependency in pom.xml

---
+++ src/main/java/com/nashtech/rootkies/constants/State.java
@@ -0,0 +1,19 @@
+package com.nashtech.rootkies.constants;
+
+public class State {
+    // Asset
+    public static final Short AVAILABLE = 1;
+    public static final Short NOT_AVAILABLE = 2;
+    public static final Short ASSIGNED = 3;
+    public static final Short WAITING_FOR_RECYCLING = 4;
+    public static final Short RECYLED = 5;
+
+    // Assignment
+    public static final Short ACCEPTED = 1;
+    public static final Short WAITING_FOR_ACCEPTANCE = 2;
+    public static final Short DECLINED = 3;
+
+    // Request
+    public static final Short WAITING_FOR_RETURNING = 1;
+    public static final Short COMPLETED = 2;
+}
