926683273cc3f26147f83664f5f7d84341e588df
CuongNC <you@example.com>
2021-08-05 10:18:32 +0700

    create sample data, fix erole, remove assetname unique contraint

--- src/main/java/com/nashtech/rootkies/enums/ERole.java
+++ src/main/java/com/nashtech/rootkies/enums/ERole.java
@@ -1,7 +1,5 @@
 package com.nashtech.rootkies.enums;
 
 public enum ERole {
-	ROLE_USER,
-    ROLE_ADMINISTRATOR,
-    ROLE_SUPER_ADMINISTRATOR
+    ROLE_USER, ROLE_ADMIN, ROLE_USER_LOCKED, ROLE_ADMIN_LOCKED
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,95 +1,216 @@
-INSERT INTO roles(id , name) VALUES(1001, 'ROLE_USER');
-INSERT INTO roles(id , name) VALUES(1002 , 'ROLE_SUPER_ADMINISTRATOR');
-INSERT INTO roles(id , name) VALUES(1003 , 'ROLE_ADMINISTRATOR');
+INSERT INTO public.roles
+    (roleid , rolename)
+VALUES(1001, 'ROLE_USER');
+INSERT INTO public.roles
+    (roleid , rolename)
+VALUES(1002, 'ROLE_ADMIN');
+INSERT INTO public.roles
+    (roleid , rolename)
+VALUES(1003, 'ROLE_USER_LOCKED');
+INSERT INTO public.roles
+    (roleid , rolename)
+VALUES(1004, 'ROLE_ADMIN_LOCKED');
+
+INSERT INTO public.locations
+    (locationid , address)
+VALUES(101, 'HCM');
+INSERT INTO public.locations
+    (locationid , address)
+VALUES(102, 'HN');
 
 INSERT INTO public.categories
-(id, created_date, description, is_deleted, "name", updated_date, image_id, parent_id)
-VALUES(18, '2021-07-26 21:19:21.108', 'Laptop - thiết bị IT', false, 'Laptop - thiết bị IT', NULL, NULL, NULL);
+    (categorycode , categoryname)
+VALUES('BM', 'Bluetooth Mouse');
 INSERT INTO public.categories
-(id, created_date, description, is_deleted, "name", updated_date, image_id, parent_id)
-VALUES(19, '2021-07-26 21:19:39.729', 'Máy ảnh quay phim', false, 'Máy ảnh quay phim', NULL, NULL, NULL);
+    (categorycode , categoryname)
+VALUES('PC', 'Personal Computer');
 INSERT INTO public.categories
-(id, created_date, description, is_deleted, "name", updated_date, image_id, parent_id)
-VALUES(20, '2021-07-26 21:20:09.134', 'Điện gia dụng', false, 'Điện gia dụng', NULL, NULL, NULL);
+    (categorycode , categoryname)
+VALUES('PJ', 'Projector');
 INSERT INTO public.categories
-(id, created_date, description, is_deleted, "name", updated_date, image_id, parent_id)
-VALUES(21, '2021-07-26 21:20:27.643', 'Nhà cửa đời sống', false, 'Nhà cửa đời sống', NULL, NULL, NULL);
+    (categorycode , categoryname)
+VALUES('MO', 'Monitor');
 INSERT INTO public.categories
-(id, created_date, description, is_deleted, "name", updated_date, image_id, parent_id)
-VALUES(22, '2021-07-26 21:21:04.070', 'Hàng tiêu dùng - thực phẩm', false, 'Hàng tiêu dùng - thực phẩm', NULL, NULL, NULL);
-INSERT INTO public.categories
-(id, created_date, description, is_deleted, "name", updated_date, image_id, parent_id)
-VALUES(23, '2021-07-26 21:21:21.130', 'Làm đẹp - sức khỏe', false, 'Làm đẹp - sức khỏe', NULL, NULL, NULL);
-INSERT INTO public.categories
-(id, created_date, description, is_deleted, "name", updated_date, image_id, parent_id)
-VALUES(24, '2021-07-26 21:21:41.508', 'Thời trang - phụ kiện', false, 'Thời trang - phụ kiện', NULL, NULL, NULL);
-INSERT INTO public.categories
-(id, created_date, description, is_deleted, "name", updated_date, image_id, parent_id)
-VALUES(25, '2021-07-26 21:22:08.272', 'Xe máy - ôtô - xe đạp', false, 'Xe máy - ôtô - xe đạp', NULL, NULL, NULL);
-INSERT INTO public.categories
-(id, created_date, description, is_deleted, "name", updated_date, image_id, parent_id)
-VALUES(26, '2021-07-26 21:22:40.248', 'Sách - vpp - quà tặng', false, 'Sách - vpp - quà tặng', NULL, NULL, NULL);
-INSERT INTO public.categories
-(id, created_date, description, is_deleted, "name", updated_date, image_id, parent_id)
-VALUES(8, '2021-07-26 20:49:55.715', 'Điện tử - điện lạnh', false, 'Điện tử - điện lạnh', NULL, NULL, NULL);
-INSERT INTO public.categories
-(id, created_date, description, is_deleted, "name", updated_date, image_id, parent_id)
-VALUES(9, '2021-07-26 20:50:04.874', 'Điện thoại - máy tính bảng', false, 'Điện thoại - máy tính bảng', NULL, NULL, NULL);
+    (categorycode , categoryname)
+VALUES('LA', 'Laptop');
 
-INSERT INTO public.address
-(id, city, country, created_date, district, is_deleted, street_address, updated_date, ward)
-VALUES(3, 'ho chi minh', 'viet nam', NULL, '1', false, 'Tran hung dao', NULL, 'pham ngu lao');
-INSERT INTO public.address
-(id, city, country, created_date, district, is_deleted, street_address, updated_date, ward)
-VALUES(5, 'ho chi minh', 'viet nam', NULL, 'tan binh', false, 'cong Hoa', NULL, '2');
-INSERT INTO public.address
-(id, city, country, created_date, district, is_deleted, street_address, updated_date, ward)
-VALUES(7, 'ho chi minh', 'viet nam', NULL, 'tan binh', false, 'cong Hoa', NULL, '2');
-
-
-INSERT INTO public.organizations
-(id, created_date, "name", address_id, image_id)
-VALUES(2, NULL, 'Samsung', 3, NULL);
-INSERT INTO public.organizations
-(id, created_date, "name", address_id, image_id)
-VALUES(4, NULL, 'Apple', 5, NULL);
-INSERT INTO public.organizations
-(id, created_date, "name", address_id, image_id)
-VALUES(6, NULL, 'Sony', 7, NULL);
-
-
-INSERT INTO public.brands
-(id, is_deleted, "name", updated_date, organization_id)
-VALUES(29, false, 'Apple', NULL, 4);
-INSERT INTO public.brands
-(id, is_deleted, "name", updated_date, organization_id)
-VALUES(30, false, 'Samsung', NULL, 4);
-
-
-INSERT INTO public.brands_categories
-(brand_id, category_id)
-VALUES(29, 8);
-INSERT INTO public.brands_categories
-(brand_id, category_id)
-VALUES(29, 9);
-INSERT INTO public.brands_categories
-(brand_id, category_id)
-VALUES(30, 9);
-INSERT INTO public.brands_categories
-(brand_id, category_id)
-VALUES(30, 8);
-
-INSERT INTO public.users
-(id, created_date, email, is_deleted, "password", phone, status, updated_date, username)
-VALUES(1, '2021-07-30 07:14:20.079', 'anhnguyencm@gmail.com', false, '$2a$10$yy5aQx41CL9mFAto15dD.OcOvUQgPV04iqtShs6RoStiSdEY2i5Jq', NULL, 'INACTIVE', NULL, 'anhnguyencm');
-INSERT INTO public.users
-(id, created_date, email, is_deleted, "password", phone, status, updated_date, username)
-VALUES(2, '2021-07-30 07:14:20.079', 'leonguyencm1984@gmail.com', false, '$2a$10$yy5aQx41CL9mFAto15dD.OcOvUQgPV04iqtShs6RoStiSdEY2i5Jq', NULL, 'INACTIVE', NULL, 'leonguyencm1984');
-
-
-INSERT INTO public.user_roles
-(user_id, role_id)
-VALUES(1, 1002);
-INSERT INTO public.user_roles
-(user_id, role_id)
-VALUES(2, 1003);
+insert
+	into
+	public.assets
+    (assetcode ,
+    assetname,
+    installdate,
+    isdeleted,
+    specification,
+    state,
+    categorycode,
+    locationid)
+values('LA000001',
+        'Laptop HP Probook 450 G1',
+        '2021-07-06 14:24:05',
+        false,
+        'intel core i3',
+        1,
+        'LA',
+        101
+);
+insert
+	into
+	public.assets
+    (assetcode ,
+    assetname,
+    installdate,
+    isdeleted,
+    specification,
+    state,
+    categorycode,
+    locationid)
+values('LA000002',
+        'Laptop HP Probook 450 G1',
+        '2021-07-06 14:25:05',
+        false,
+        'intel core i5',
+        1,
+        'LA',
+        101
+);
+insert
+	into
+	public.assets
+    (assetcode ,
+    assetname,
+    installdate,
+    isdeleted,
+    specification,
+    state,
+    categorycode,
+    locationid)
+values('LA000003',
+        'Laptop HP Probook 450 G1',
+        '2021-07-06 14:26:05',
+        false,
+        'intel core i7',
+        2,
+        'LA',
+        101
+);
+insert
+	into
+	public.assets
+    (assetcode ,
+    assetname,
+    installdate,
+    isdeleted,
+    specification,
+    state,
+    categorycode,
+    locationid)
+values('LA000004',
+        'Laptop HP Probook 450 G1',
+        '2021-07-06 14:24:05',
+        false,
+        'intel core i3',
+        1,
+        'LA',
+        102
+);
+insert
+	into
+	public.assets
+    (assetcode ,
+    assetname,
+    installdate,
+    isdeleted,
+    specification,
+    state,
+    categorycode,
+    locationid)
+values('LA000005',
+        'Laptop HP Probook 450 G1',
+        '2021-07-06 14:25:05',
+        false,
+        'intel core i5',
+        1,
+        'LA',
+        102
+);
+insert
+	into
+	public.assets
+    (assetcode ,
+    assetname,
+    installdate,
+    isdeleted,
+    specification,
+    state,
+    categorycode,
+    locationid)
+values('LA000006',
+        'Laptop HP Probook 450 G1',
+        '2021-07-06 14:26:05',
+        false,
+        'intel core i7',
+        1,
+        'LA',
+        102
+);
+insert
+	into
+	public.assets
+    (assetcode ,
+    assetname,
+    installdate,
+    isdeleted,
+    specification,
+    state,
+    categorycode,
+    locationid)
+values('PC000001',
+        'Personal Computer',
+        '2021-07-06 14:24:05',
+        false,
+        'intel core i3',
+        1,
+        'PC',
+        101
+);
+insert
+	into
+	public.assets
+    (assetcode ,
+    assetname,
+    installdate,
+    isdeleted,
+    specification,
+    state,
+    categorycode,
+    locationid)
+values('PC000002',
+        'Personal Computer',
+        '2021-07-06 14:25:05',
+        false,
+        'intel core i5',
+        1,
+        'PC',
+        101
+);
+insert
+	into
+	public.assets
+    (assetcode ,
+    assetname,
+    installdate,
+    isdeleted,
+    specification,
+    state,
+    categorycode,
+    locationid)
+values('PC000003',
+        'Personal Computer',
+        '2021-07-06 14:26:05',
+        false,
+        'intel core i7',
+        2,
+        'PC',
+        101
+);
