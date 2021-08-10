b72576f517a8dd3255b0f2d6b5cbe3935825ec42
CuongNC <nccuong281299@gmail.com>
2021-08-10 18:26:41 +0700

    add preauthorise to view asset controller and sample data for login

--- src/main/java/com/nashtech/rootkies/controllers/CategoryController.java
+++ src/main/java/com/nashtech/rootkies/controllers/CategoryController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -25,6 +26,7 @@ public class CategoryController {
     }
 
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ResponseDTO> retrieveCategories() throws DataNotFoundException {
         return ResponseEntity.ok(categoryService.retrieveCategories());
     }
