8c700e765cd80466d47d7d4d01b26f8402ef9d01
Hưng Nguyễn <hungamtp@gmail.com>
2021-08-10 21:36:53 +0700

    test userrepo.findAll(spec , pageable)

--- src/test/java/com/nashtech/rootkies/repository/UserRepositotyTest.java
+++ src/test/java/com/nashtech/rootkies/repository/UserRepositotyTest.java
@@ -2,10 +2,14 @@ package com.nashtech.rootkies.repository;
 
 import com.nashtech.rootkies.model.User;
 import com.nashtech.rootkies.repository.UserRepository;
+import com.nashtech.rootkies.repository.specs.SearchCriteria;
+import com.nashtech.rootkies.repository.specs.UserSpecification;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
@@ -26,5 +30,14 @@ public class UserRepositotyTest {
         String username = user.get().getUsername();
         Boolean check = userRepository.existsByUsername(username);
         assertEquals(check, true);
+    }
+
+    @Test
+    public void findAllUser(){
+        Specification specification = new UserSpecification(
+                new SearchCriteria("staffCode" , ":" , "SD0001"));
+
+        assertEquals(userRepository.findAll(specification , PageRequest.of(0 , 1)).getTotalElements() , 1);
+
     }
 }
