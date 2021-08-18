cdcab97aadb92c0e617d54310786f076a17731b9
Hưng Nguyễn <hungamtp@gmail.com>
2021-08-18 11:00:41 +0700

    no need get staffcode of assignedBy from Ui

--- src/main/java/com/nashtech/rootkies/dto/assignment/response/ViewAssignmentDTO.java
+++ src/main/java/com/nashtech/rootkies/dto/assignment/response/ViewAssignmentDTO.java
@@ -3,11 +3,17 @@ package com.nashtech.rootkies.dto.assignment.response;
 import java.time.LocalDateTime;
 import java.util.Collection;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ViewAssignmentDTO {
     private Long assignmentId;
 
